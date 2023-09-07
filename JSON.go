53418e435f96e0f68a3c5f77e25cbed1d2fa01fa
Iisak <iisakvirma@gmail.com>
2023-09-07 09:48:52 +0300

    handlers

--- JSON.go
+++ JSON.go
@@ -9,11 +9,11 @@ import (
 func RespondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	data, err := json.Marshal(payload)
 	if err != nil {
-		log.Println("Failed to marshal JSON response: %v", payload)
+		log.Printf("Failed to marshal JSON response: %v", payload)
 		w.WriteHeader(500)
 		return
 	}
 	w.Header().Add("Content-Type", "Application/json")
-	w.WriteHeader(200)
+	w.WriteHeader(code)
 	w.Write(data)
 }
